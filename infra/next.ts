f3d79818abde0664c8984ebfcebf9e0c095a37fd
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2024-11-30 20:45:10 +0000

    linked db to the websites

--- infra/next.ts
+++ infra/next.ts
@@ -1,6 +1,10 @@
+import { shows } from "./db";
+
 new sst.aws.Nextjs("public", {
   path: "packages/public",
+  link: [shows],
 });
 new sst.aws.Nextjs("private", {
   path: "packages/private",
+  link: [shows],
 });
