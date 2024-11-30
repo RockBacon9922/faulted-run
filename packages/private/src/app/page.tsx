23f1bf967769a20073b5fd517a3b77e79986afe1
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2024-11-30 21:22:30 +0000

    got shows system working

--- packages/private/src/app/page.tsx
+++ packages/private/src/app/page.tsx
@@ -1,5 +1,63 @@
-import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { agility_shows } from "@/db/shows";
+
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { Resource } from "sst";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Shows />
+    </>
+  );
 }
+
+const Shows = () => {
+  return (
+    <Table>
+      <TableCaption>
+        Shows That Are Currently Tracked By The System
+      </TableCaption>
+      <ShowsTableHead />
+      <ShowsTableBody />
+    </Table>
+  );
+};
+
+const ShowsTableBody = async () => {
+  const client = new DynamoDBClient();
+  const res = await client.send(
+    new ScanCommand({ TableName: Resource.shows.name }),
+  );
+  const shows = agility_shows.parse(res.Items);
+
+  return (
+    <TableBody>
+      {shows.map((show) => (
+        <TableRow key={show.id}>
+          <td>{show.name}</td>
+        </TableRow>
+      ))}
+    </TableBody>
+  );
+};
+
+const ShowsTableHead = () => (
+  <>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Show Name</TableHead>
+        <TableHead>Scrime</TableHead>
+        <TableHead>Checker Inner</TableHead>
+      </TableRow>
+    </TableHeader>
+  </>
+);
---
+++ packages/private/src/db/shows.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const agility_fault = z.object({
+  type: z.enum(["Normal", "Refusal", "Handling", "Time"]),
+  value: z.number(),
+});
+export type AgilityFault = z.infer<typeof agility_fault>;
+
+export const agility_runner = z.object({
+  name: z.string().min(1),
+  dog_name: z.string().min(1),
+  state: z.enum(["Checked In", "Late", "Running", "Finished"]),
+  time: z.number().int().positive().min(1),
+  faults: z.array(agility_fault),
+});
+export type AgilityRunner = z.infer<typeof agility_runner>;
+
+export const agility_class = z.object({
+  name: z.string().min(1),
+  height: z.enum(["Large", "Intermediate", "Medium", "Small", "Combined"]),
+  active: z.boolean(),
+  runners: z.array(agility_runner),
+  course_time: z.number().int().positive().min(1),
+});
+export type AgilityClass = z.infer<typeof agility_class>;
+
+export const agility_show = z.object({
+  id: z.string().min(1),
+  name: z.string().min(1),
+  classes: z.array(agility_class),
+});
+export type Show = z.infer<typeof agility_show>;
+
+export const agility_shows = z.array(agility_show);
+export type Shows = z.infer<typeof agility_shows>;
